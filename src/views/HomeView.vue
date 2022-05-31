1178ea7bc02dc40abf324e258ec28511f361475c
sky2000sh <sky2000sh@gmail.com>
2022-06-01 01:49:04 +0900

    commit on 01/06/22

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <div>{{name}} - {{age}}</div>
+  <button @click="fnCangeName('clerk')">Change Name</button>
+  <button @click="fnChangeAge(13)">Change Age</button>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data () {
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  },
+
+  methods: {
+    fnCangeName (name: string) {
+      this.name = name
+      return name
+    },
+
+    fnChangeAge (age: string | number) {
+      this.age = age
+      return age
+    }
   }
 })
 export default class HomeView extends Vue {}
