db1e639ae9f829130d6d00f4ef1b3e01132daa2b
sky2000sh <sky2000sh@gmail.com>
2022-06-01 13:23:03 +0900

    4th commit on 01/06/22

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,15 @@
 
     <!-- <p>{{ jobs[0].location }}</p> -->
 
-    <JobsList :jobs="jobs"/>
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+
+    <JobsList :jobs="jobs" :order="order"/>
   </div>
 </template>
 
@@ -42,6 +50,7 @@
 import { defineComponent, reactive, ref, toRefs } from 'vue'
 import JobsList from '../views/JobsList.vue'
 import Job from '../types/index'
+import OrderTerm from '../types/orderTerm'
 
 export default defineComponent({
   // name: 'App',
@@ -80,7 +89,12 @@ export default defineComponent({
       { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: 'GY1005' }
     ])
 
-    return { jobs }
+    const order = ref<OrderTerm>('title')
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
   }
 
   // methods: {
@@ -97,3 +111,24 @@ export default defineComponent({
 })
 
 </script>
+
+<style>
+  header {
+    text-align: center;
+  }
+
+  header .order {
+    margin-top: 20px;
+  }
+
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+</style>
